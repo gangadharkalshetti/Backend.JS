25c415927f0e8fe76263a4e30a7b1d9900d299b7
gnagadhar kalshetti <gangadharkalshetti16@gmail.com>
2025-08-16 18:32:03 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
+
 const app = express()  
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    Credentials: true    
+    credentials: true    
 }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("puclic"))
+app.use(express.static("public"))
 app.use(cookieParser())
+
+
+
+// router import
+import userRouter from "./routes/user.routes.js"
+
+
+
+
+// router declaration 
+app.use("/api/v1/users", userRouter)
 
 export default app
